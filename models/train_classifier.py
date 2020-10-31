2fedf3592c4a82d1bacc30825d3d0db44b2ca742
jpthewes <jpthewes@gmail.com>
2020-10-31 19:49:58 +0000

    initial running scripts and web app

---
+++ models/train_classifier.py
@@ -0,0 +1,102 @@
+import sys
+import pandas as pd
+import numpy as np
+import sqlite3
+import re
+import nltk
+import pickle
+
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sqlalchemy import create_engine
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('messages_categories', engine)
+    # split in X and Y
+    X = df['message']
+    Y = df[[col for col in df.columns if col not in ['id', 'message', 'original', 'genre']]]
+    category_names = Y.columns
+
+    
+    return X, Y, category_names
+
+def tokenize(text):
+    # remove special characters
+    text = re.sub("[^a-zA-Z0-9]", " ", text)
+    
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    randomforest = RandomForestClassifier()
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(randomforest))
+    ])
+    # TODO!! params
+    return pipeline
+
+
+def evaluate_model(model, X_test, y_test, category_names):
+    y_pred = model.predict(X_test)
+    for i, c in enumerate(y_test.columns): 
+        print("category: ", c) 
+        print(classification_report(y_test.values[i], y_pred[i]))
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
